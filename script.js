c295be750cef26e9a4290c8f66c29af8b32ee2c4
FranMarapode <169156480+FranMarapode@users.noreply.github.com>
2025-03-31 19:16:38 -0300

    Fecha y hora

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const agregar = ( ) => {
     if (ingreso.value !== "") {
         tareas.push({
             texto: ingreso.value,
-            estado: false
+            estado: false,
+            fechaTachado: null,
+            fechaCreacion: new Date()
         })
         ingreso.value = ""
         mostrar()
@@ -20,10 +22,13 @@ const mostrar = () => {
 
         tareas.forEach((t, id) =>  {
             let estaCheck = t.estado ? "checked" : ""
+            let fechaC = new Date(t.fechaCreacion).toLocaleString()
             lista.innerHTML += `
             <li>
                 <input type="checkbox" id="checkbox-${id}" onchange="marcar(${id})" ${estaCheck}>
                 <span style="text-decoration: ${t.estado ? "line-through" : "none"}">${t.texto}</span>
+                <small>Creado: ${fechaC}</small>
+                <small>Tachado: ${t.fechaTachado ? t.fechaTachado : "-"}</small>
             </li>`;
         })
 
@@ -32,6 +37,8 @@ const mostrar = () => {
 
 const marcar = (id) => {
     tareas[id].estado = !tareas[id].estado
+    let fechaT = tareas[id].estado ? new Date() : null
+    tareas[id].fechaTachado = fechaT ? fechaT.toLocaleString() : null
     mostrar()
 }
 
