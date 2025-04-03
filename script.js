88be767ec32ef116115ff65b676d526370e038e9
JOAQUIN IGNACIO ORDO�EZ <48240825@ort>
2025-04-03 10:35:17 -0300

    Avanzamos

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let ingreso = document.getElementById("ingreso")
 
 
 const agregar = ( ) => {
+    let time = Date.now()
     if (ingreso.value !== "") {
         tareas.push({
             texto: ingreso.value,
             estado: false,
             fechaTachado: null,
-            fechaCreacion: new Date()
+            fechaCreacion: new Date(time)
         })
         ingreso.value = ""
         mostrar()
@@ -27,16 +28,19 @@ const mostrar = () => {
                 <span style="text-decoration: ${t.estado ? "line-through" : "none"}">${t.texto}</span>
                 <small>Creado: ${fechaC}</small>
                 <small>Tachado: ${t.fechaTachado ? t.fechaTachado : "-"}</small>
-                <button onclick="eliminar(${id})">Eliminar</button>
+                <button id="eliminar" onclick="eliminar(${id})">Eliminar</button>
             </li>`;
+           
+            
         })
 
         
     }
 
 const marcar = (id) => {
+    let time = Date.now()
     tareas[id].estado = !tareas[id].estado
-    let fechaT = tareas[id].estado ? new Date() : null
+    let fechaT = tareas[id].estado ? new Date(time) : null
     tareas[id].fechaTachado = fechaT ? fechaT.toLocaleString() : null
     mostrar()
 }
@@ -47,6 +51,24 @@ const eliminar = (id) => {
         tareas.splice(id, 1)
         mostrar()
     }
+}
+
+const CalculoMasRapido = () => {
+    let tareaMasRapido;
+    for (let i = 0; i < tareas.length; i++) {
+        if (tareas[i].fechaTachado != null) {
+            let rapidez = (tareas[i].fechaTachado - tareas[i].fechaCreacion);
+            let masRapido = ""
+            if ( masRapido == "" || rapidez < masRapido) {
+                masRapido = rapidez;
+                tareaMasRapido = tareas[i]
+            }
+        }
+        
+    }
+    mostrar()
+    alert(tareaMasRapido.texto)
+   
 }
 
 
